d76e29cbf670e29304f87236c970f5c02b165401
Vedant Chainani <vedantchainani1084@gmail.com>
2023-02-24 14:31:03 +0530

    Move constants to root

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-};
-
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.jsx",
