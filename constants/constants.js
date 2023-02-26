172463244beb11f3e21aa018046afee5e57da609
Vedant Chainani <vedantchainani1084@gmail.com>
2023-02-27 01:21:45 +0530

    Add Navigation Cards

--- constants/constants.js
+++ constants/constants.js
@@ -1,7 +1,22 @@
 export const days = [
   {
-    title: "Licenses and Comments",
+    title: "Layout of a Solidity File",
     icon: "1️⃣ ",
-    url: "/30-days-of-solidity/start/licenses-and-comments",
+    url: "/30-days-of-solidity/start/layout-of-solidity-file",
+  },
+  {
+    title: "Variables and Datatypes",
+    icon: "2️⃣ ",
+    url: "/30-days-of-solidity/start/variables-and-datatypes",
+  },
+  {
+    title: "Scopes and Operators",
+    icon: "3️⃣ ",
+    url: "/30-days-of-solidity/start/scopes-and-operators",
+  },
+  {
+    title: "Functions",
+    icon: "4️⃣ ",
+    url: "/30-days-of-solidity/start/functions",
   },
 ];
