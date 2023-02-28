999b0e567912adfba5dba6ebf98d9f9dc087b67f
Vedant Chainani <vedantchainani1084@gmail.com>
2023-03-01 01:46:25 +0530

    Fix small error in Object Reference

--- theme.config.jsx
+++ theme.config.jsx
@@ -98,10 +98,12 @@ const config = {
         return seo;
       } else if (slugs.length == 2) {
         const seo = SEO[slugs[1]];
-        return seo;
+        const img = seo.image();
+        return { ...seo, image: img };
       } else if (slugs.length > 2) {
         const seo = SEO[slugs[1]][slugs[slugs.length - 1]];
-        return seo;
+        const img = seo.image();
+        return { ...seo, image: img };
       }
     };
 
@@ -122,13 +124,13 @@ const config = {
         <meta name="twitter:title" content={route === "/" ? "Guides" : title} />
         <meta name="twitter:description" content={seo.description} />
         <meta name="twitter:creator" content="@Envoy_1084" />
-        <meta name="twitter:image" content={seo.image()} />
+        <meta name="twitter:image" content={seo.image} />
 
         {/* Open Graph data */}
         <meta property="og:locale" content="en_US" />
         <meta property="og:title" content={route === "/" ? "Guides" : title} />
         <meta property="og:url" content="https://guides.vedantc.dev" />
-        <meta property="og:image" content={seo.image()} />
+        <meta property="og:image" content={seo.image} />
         <meta property="og:image:type" content="image/png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
